260e2229bfa15c128f284579f0decf870a7efbf2
imcheesed <LeK7120@sduhsd.lan>
2016-09-02 13:37:07 -0700

    fixed ASCII2

--- APLesson_01/ASCII2.java
+++ APLesson_01/ASCII2.java
@@ -2,7 +2,16 @@ public class Lab_01_ASCII_2
 {
 	public static void main(String[]args)
 	{
-		System.out.println("..");
-		System.out.println("---");
+		System.out.println("**********_________________________");
+		System.out.println("**********_________________________");
+		System.out.println("**********_________________________");
+		System.out.println("**********_________________________");
+		System.out.println("**********_________________________");
+		System.out.println("___________________________________");
+		System.out.println("___________________________________");
+		System.out.println("___________________________________");
+		System.out.println("___________________________________");
+		System.out.println("___________________________________");
+		
 	}
 }
