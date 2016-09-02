83e2391537b5876be0fac74112dffc0511b01783
Kien Le <LeK7120@sduhsd.lan>
2016-09-02 13:12:22 -0700

    Added Files

---
+++ APLesson_01/ASCII1.java
@@ -0,0 +1,9 @@
+public class Lab_01_ASCII_1
+{
+	public static void main(String[]args)
+	{
+		System.out.println(" n._.n");
+		System.out.println(" `o o'");
+		System.out.println("(~@~)");
+	}
+}
---
+++ APLesson_01/ASCII2.java
@@ -0,0 +1,8 @@
+public class Lab_01_ASCII_2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("..");
+		System.out.println("---");
+	}
+}
