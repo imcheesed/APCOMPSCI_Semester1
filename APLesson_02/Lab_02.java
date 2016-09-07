1dc4b057f025dd6e55aa50e0754ad5a00790ae3b
imcheesed <LeK7120@sduhsd.lan>
2016-09-07 14:15:03 -0700

    Added APLesson_02

---
+++ APLesson_02/Lab_02.java
@@ -0,0 +1,22 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int a = 3;
+		int b = 11;
+		System.out.println("The product of 3 and 11 is " + a * b);
+		
+		String Name = "Kien Le";
+		String Address = "1600 Pennsylvania Avenue";
+		String City = "Washington, D.C, 20500";
+		System.out.println(Name);
+		System.out.println(Address);
+		System.out.println(City);
+		
+		int l = 2;
+		int w = 6;
+		int h = 5;
+		int SurfaceArea = 2*l*w+2*l*h+2*w*h;
+		System.out.println("The surface area of your rectangle is " + SurfaceArea + ".");
+	}
+}
