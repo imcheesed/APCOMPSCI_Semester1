9587a144e2e932905ff134bc7a99507e72e8e6e5
imcheesed <fallingupkl@gmail.com>
2016-09-23 14:29:20 -0700

    class

--- APLesson_01/ASCII1.java
+++ APLesson_01/ASCII1.java
@@ -2,8 +2,8 @@ public class ASCII1
 {
 	public static void main(String[]args)
 	{
-		System.out.println(" n._.n");
-		System.out.println(" `o o'");
-		System.out.println(" (~@~)");
+		System.out.println("n._.n");
+		System.out.println("`o o'");
+		System.out.println("(~@~)");
 	}
 }
